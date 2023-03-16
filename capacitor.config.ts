bd371e37493639b767ad36b0d7c60ead8929474e
akapayev <akapayev@greet-go.com>
2023-03-16 20:17:59 +0600

    feat: init app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'kz.roommate.mobile',
+  appName: 'Roommate',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
