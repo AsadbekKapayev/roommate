2a633c0801b610c18feb8d8eef486b582dbc9c05
Kapayev Assabek <assadbekkapayev@gmail.com>
2023-03-19 13:17:26 +0600

    feat: custom input tel created

--- src/app/components/custom-input/custom-module.module.ts
+++ src/app/components/custom-input/custom-module.module.ts
@@ -4,19 +4,23 @@ import {CustomInputComponent} from "./custom-input.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {PipesModule} from "../../pipes/cores/pipes.module";
 import {ErrorMessageFormModule} from "../error-messge-form/error-message-form.module";
+import {NgxMaskModule} from "ngx-mask";
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 @NgModule({
-    declarations: [CustomInputComponent],
-    exports: [
-        CustomInputComponent
-    ],
+  declarations: [CustomInputComponent],
+  exports: [
+    CustomInputComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     PipesModule,
     ErrorMessageFormModule,
-  ]
+    NgxMaskModule.forRoot(),
+    MatFormFieldModule,
+  ],
 })
 export class CustomModuleModule {
 }
