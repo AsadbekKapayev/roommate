413ce0b83615b43a0e4de0b5dba4d83072fee10b
Kapayev Assabek <assadbekkapayev@gmail.com>
2023-04-02 20:39:04 +0600

    feat: profile setting modal init2

---
+++ src/app/models/commons/ProfileSetting.ts
@@ -0,0 +1,11 @@
+import {Gender} from "./Gender";
+
+export class ProfileSetting {
+  photoId: string;
+  surname: string;
+  name: string;
+  patronymic: string;
+  birthDate: string;
+  phoneNumber: string;
+  gender: Gender;
+}
