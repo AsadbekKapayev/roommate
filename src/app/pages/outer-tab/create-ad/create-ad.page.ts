a3a6ef7c0f687b13719e460b3446ccf8f8577d13
Kapayev Assabek <assadbekkapayev@gmail.com>
2023-04-16 14:09:39 +0600

    feat: ymap added

--- src/app/pages/outer-tab/create-ad/create-ad.page.ts
+++ src/app/pages/outer-tab/create-ad/create-ad.page.ts
@@ -4,6 +4,8 @@ import {LoginService} from "../../../services/core/login.service";
 import {ActivatedRoute} from "@angular/router";
 import {AdService} from "../../../services/common/ad.service";
 
+import ymaps from 'ymaps';
+
 @Component({
   selector: 'app-create-ad',
   templateUrl: './create-ad.page.html',
@@ -14,7 +16,7 @@ export class CreateAdPage implements OnInit {
   selectedRoomQuantity: string;
   roomsQuantity: string[] = [
     '1', '2', '3', '4', '5+'
-  ]
+  ];
 
   constructor(private navCtrl: NavController,
               private loginService: LoginService,
@@ -23,6 +25,23 @@ export class CreateAdPage implements OnInit {
   }
 
   ngOnInit() {
+    // todo should I use ymap.ready()
+    this.loadMap().then();
+  }
+
+  async loadMap() {
+    const app = document.getElementById("map");
+    const maps = await ymaps.load();
+    const mapContainer = document.createElement("div");
+    mapContainer.style.height = "200px";
+    mapContainer.style.width = "100%";
+    app.appendChild(mapContainer);
+    const map = new maps.Map(mapContainer, {
+      center: [55.76, 37.64],
+      zoom: 11
+    }, {
+      searchControlProvider: 'yandex#search'
+    });
   }
 
   onClickBackIcon() {
