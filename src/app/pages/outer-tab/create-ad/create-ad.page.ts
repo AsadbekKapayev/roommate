dbdd599ada12ce9876b37681e88f347532c8a9fe
Kapayev Assabek <assadbekkapayev@gmail.com>
2023-05-04 21:44:56 +0600

    feat: create ad

--- src/app/pages/outer-tab/create-ad/create-ad.page.ts
+++ src/app/pages/outer-tab/create-ad/create-ad.page.ts
@@ -7,6 +7,8 @@ import {AdService} from "../../../services/common/ad.service";
 import ymaps from 'ymaps';
 import {SettingControllerService} from "../../../services/controllers/setting-controller.service";
 import {FilterType} from "../../../models/commons/ad/FilterType";
+import {Item} from "../../../models/commons/Item";
+import {isEmpty} from "../../../shares/cores/util-method";
 
 @Component({
   selector: 'app-create-ad',
@@ -19,6 +21,23 @@ export class CreateAdPage implements OnInit {
   roomsQuantity: string[] = [
     '1', '2', '3', '4', '5+'
   ];
+
+  price_from: number;
+  price_to: number;
+
+  selectedCity: Item[];
+  selectedGenderType: Item[];
+  selectedApartmentConditions: Item[];
+  selectedApartmentFurniture: Item[];
+  selectedApartmentFacilities: Item[];
+  selectedApartmentBathroomTypes: Item[];
+  selectedApartmentFurnitureStatuses: Item[];
+  selectedApartmentBathrooms: Item[];
+  selectedApartmentSecurities: Item[];
+  selectedWindowDirections: Item[];
+  selectedApartmentFor: Item[];
+
+  type = FilterType;
 
   constructor(private navCtrl: NavController,
               private loginService: LoginService,
@@ -55,9 +74,91 @@ export class CreateAdPage implements OnInit {
     this.navCtrl.back();
   }
 
-  onClick(title?: string, code?: FilterType) {
-    this.settingControllerService.setSelectModal(title, code).presentSecondary().then();
-    console.log('V7T12Gdl :: ')
+  onClick(title?: string, code?: FilterType, isCheckboxModal?: boolean) {
+    if (isCheckboxModal) {
+      this.settingControllerService.setCheckboxModal(title, code, this.getItemsByCode(code)).presentSecondary().then(x => {
+        if (!x?.data || isEmpty(x?.data)) {
+          return;
+        }
+
+        this.setItemsByCode(code, x.data);
+      });
+
+      return;
+    }
+
+    this.settingControllerService.setSelectModal(title, code, this.getItemsByCode(code)).presentSecondary().then(x => {
+      if (!x?.data || isEmpty(x?.data)) {
+        return;
+      }
+
+      this.setItemsByCode(code, [x.data]);
+    });
+  }
+
+  getItemsByCode(code: FilterType) {
+    switch (code) {
+      case FilterType.CITY:
+        return this.selectedCity;
+      case FilterType.GENDER_TYPE:
+        return this.selectedGenderType;
+      case FilterType.APARTMENT_CONDITIONS:
+        return this.selectedApartmentConditions;
+      case FilterType.APARTMENT_FURNITURE:
+        return this.selectedApartmentFurniture;
+      case FilterType.APARTMENT_FACILITIES:
+        return this.selectedApartmentFacilities;
+      case FilterType.APARTMENT_BATHROOM_TYPES:
+        return this.selectedApartmentBathroomTypes;
+      case FilterType.APARTMENT_FURNITURE_STATUSES:
+        return this.selectedApartmentFurnitureStatuses;
+      case FilterType.APARTMENT_BATHROOMS:
+        return this.selectedApartmentBathrooms;
+      case FilterType.APARTMENT_SECURITIES:
+        return this.selectedApartmentSecurities;
+      case FilterType.WINDOW_DIRECTIONS:
+        return this.selectedWindowDirections;
+      case FilterType.APARTMENT_FOR:
+        return this.selectedApartmentFor;
+    }
+  }
+
+  setItemsByCode(code: FilterType, values: Item[]) {
+    switch (code) {
+      case FilterType.CITY:
+        this.selectedCity = values;
+        return;
+      case FilterType.GENDER_TYPE:
+        this.selectedGenderType = values;
+        return;
+      case FilterType.APARTMENT_CONDITIONS:
+        this.selectedApartmentConditions = values;
+        return;
+      case FilterType.APARTMENT_FURNITURE:
+        this.selectedApartmentFurniture = values;
+        return;
+      case FilterType.APARTMENT_FACILITIES:
+        this.selectedApartmentFacilities = values;
+        return;
+      case FilterType.APARTMENT_BATHROOM_TYPES:
+        this.selectedApartmentBathroomTypes = values;
+        return;
+      case FilterType.APARTMENT_FURNITURE_STATUSES:
+        this.selectedApartmentFurnitureStatuses = values;
+        return;
+      case FilterType.APARTMENT_BATHROOMS:
+        this.selectedApartmentBathrooms = values;
+        return;
+      case FilterType.APARTMENT_SECURITIES:
+        this.selectedApartmentSecurities = values;
+        return;
+      case FilterType.WINDOW_DIRECTIONS:
+        this.selectedWindowDirections = values;
+        return;
+      case FilterType.APARTMENT_FOR:
+        this.selectedApartmentFor = values;
+        return;
+    }
   }
 
 }
